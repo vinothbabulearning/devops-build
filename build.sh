2c9d1a835729d445b9b79d0ec3ab3c03a02b80f7
Vinoth <vinothbabulearning@gmail.com>
2024-03-19 10:59:27 +0530

    dev

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Building docker image
+docker build -t vinothbabulearning/dev:v2 .
+
+# creating container using docker compose file
+docker compose up -d
+
+
---
+++ deploy.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+docker login -u vinothbabulearning
+docker push vinothbabulearning/dev:v2
