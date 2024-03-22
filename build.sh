754eb49c44722516db362033de315ace963bfa8d
Vinoth <vinothbabulearning@gmail.com>
2024-03-22 22:55:42 +0530

    dev

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ docker rm $(docker ps -a -q)
 docker ps -a
 
 # Building docker image
-docker build -t vinothbabulearning/$1:v2 .
+docker build -t vinothbabulearning/$1:$2 .
 
 ### creating container using docker compose file
 docker compose up -d
